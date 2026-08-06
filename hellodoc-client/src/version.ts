eee328883d0a1142a3d5756674113f26252392cb
liyc <liyc@epoint.com.cn>
2026-08-06 15:36:10 +0800

    chore: 发布版本 v2.0.6

--- hellodoc-client/src/version.ts
+++ hellodoc-client/src/version.ts
@@ -1,7 +1,7 @@
 // 本文件由 scripts/release.js 自动生成，请勿手动修改
-export const APP_VERSION = '2.0.5';
-export const BUILD_TIME = '2026/8/6 13:48:27';
-export const GIT_COMMIT = '9fe8289';
+export const APP_VERSION = '2.0.6';
+export const BUILD_TIME = '2026/8/6 15:36:10';
+export const GIT_COMMIT = 'af70837';
 
 export default {
   version: APP_VERSION,
